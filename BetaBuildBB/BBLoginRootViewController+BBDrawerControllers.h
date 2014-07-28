b1cb5352487f8658ee52d1d00510ac689d6b5da5
Viktor Falkner <vfalkner@live.unc.edu>
2014-07-28 11:57:50 -0400

    Created Category to clean up Login View Controller

---
+++ BetaBuildBB/BBLoginRootViewController+BBDrawerControllers.h
@@ -0,0 +1,13 @@
+//
+//  BBLoginRootViewController+BBDrawerControllers.h
+//  BetaBuildBB
+//
+//  Created by Viktor Falkner on 7/28/14.
+//
+//
+
+#import "BBLoginRootViewController.h"
+
+@interface BBLoginRootViewController (BBDrawerControllers)
+
+@end
---
+++ BetaBuildBB/BBLoginRootViewController+BBDrawerControllers.m
@@ -0,0 +1,104 @@
+//
+//  BBLoginRootViewController+BBDrawerControllers.m
+//  BetaBuildBB
+//
+//  Created by Viktor Falkner on 7/28/14.
+//
+//
+
+#import "BBLoginRootViewController+BBDrawerControllers.h"
+
+@implementation BBLoginRootViewController (BBDrawerControllers)
+
+#pragma mark - PFLoginViewControllerDelegate
+
+// Sent to the delegate to determine whether the log in request should be submitted to the server.
+- (BOOL)logInViewController:(PFLogInViewController *)logInController shouldBeginLogInWithUsername:(NSString *)username password:(NSString *)password
+{
+    if (username && password && username.length != 0 && password.length != 0) {
+        return YES; // Begin login process
+    }
+    
+    [[[UIAlertView alloc] initWithTitle:@"Missing Information"
+                                message:@"Make sure you fill out all of the information!"
+                               delegate:nil
+                      cancelButtonTitle:@"ok"
+                      otherButtonTitles:nil] show];
+    return NO; // Interrupt login process
+}
+
+// Sent to the delegate when a PFUser is logged in.
+- (void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+// Sent to the delegate when the log in attempt fails.
+- (void)logInViewController:(PFLogInViewController *)logInController didFailToLogInWithError:(NSError *)error
+{
+    NSLog(@"Failed to log in...");
+}
+
+// Sent to the delegate when the log in screen is dismissed.
+- (void)logInViewControllerDidCancelLogIn:(PFLogInViewController *)logInController
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+#pragma mark - PFSignUpViewControllerDelegate
+
+// Sent to the delegate to determine whether the sign up request should be submitted to the server.
+- (BOOL)signUpViewController:(PFSignUpViewController *)signUpController shouldBeginSignUp:(NSDictionary *)info
+{
+    BOOL informationComplete = YES;
+    // loop through all of the submitted data
+    for (id key in info) {
+        NSString *field = [info objectForKey:key];
+        if (!field || field.length == 0) { // check completion
+            informationComplete = NO;
+            break;
+        }
+    }
+    // Display an alert if a field wasn't completed
+    if (!informationComplete) {
+        [[[UIAlertView alloc] initWithTitle:@"Missing Information"
+                                    message:@"Make sure you fill out all of the information!"
+                                   delegate:nil
+                          cancelButtonTitle:@"ok"
+                          otherButtonTitles:nil] show];
+    }
+    return informationComplete;
+}
+
+// Sent to the delegate when a PFUser is signed up.
+- (void)signUpViewController:(PFSignUpViewController *)signUpController didSignUpUser:(PFUser *)user
+{
+    [self dismissViewControllerAnimated:YES completion:nil]; // Dismiss the PFSignUpViewController
+}
+
+// Sent to the delegate when the sign up attempt fails.
+- (void)signUpViewController:(PFSignUpViewController *)signUpController didFailToSignUpWithError:(NSError *)error
+{
+    NSLog(@"Failed to sign up...");
+}
+
+// Sent to the delegate when the sign up screen is dismissed.
+- (void)signUpViewControllerDidCancelSignUp:(PFSignUpViewController *)signUpController
+{
+    NSLog(@"User dismissed the signUpViewController");
+}
+
+#pragma mark - Left Button
+-(void)leftDrawerButtonPress:(id)sender{
+    [self.mm_drawerController toggleDrawerSide:MMDrawerSideLeft animated:YES completion:nil];
+    
+}
+
+#pragma mark - Right Button
+-(void)rightDrawerButtonPress:(id)sender{
+    [self.mm_drawerController toggleDrawerSide:MMDrawerSideRight animated:YES completion:nil];
+    
+}
+
+
+@end
